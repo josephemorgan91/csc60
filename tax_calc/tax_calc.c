b6627a2c4c43ceb7d245893ed82c3bda2423e003
Joe Morgan <josephemorgan91@gmail.com>
2020-02-18 02:11:11 +0000

    Removed function to comply with assignment specification

--- tax_calc/tax_calc.c
+++ tax_calc/tax_calc.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-	
+
 enum bracket_cutoff {c_first = 9525, c_second = 38700, c_third = 82500, c_fourth = 100000}; // Want to keep constants isolated
 enum bracket_percent {p_first = 10, p_second = 12, p_third = 22, p_fourth = 24};
 
@@ -12,29 +12,21 @@ enum bracket_percent {p_first = 10, p_second = 12, p_third = 22, p_fourth = 24};
 double get_total_tax (double);
 
 int main() {
-	unsigned int user_income;
-	double total_tax;
+	unsigned int user_income = 0;
+	double total_tax = 0;
 	printf ("Please enter you annual salary as a whole number: ");
 	scanf ("%d", &user_income);
 	printf ("\n");
-
-	printf("Your total taxes are: $%0.2f\n", get_total_tax ((double)user_income));
-}
-
-double get_total_tax (double income) {
-
-	double total_tax = 0;
-
-	if (income < c_first)
-		total_tax = income * ((double)p_first / 100);
-	else if (income < c_second)
-		total_tax = TIER_10_TAX + ((income - c_first) * ((double)p_second / 100));
-	else if (income < c_third)
-		total_tax = TIER_10_TAX + TIER_12_TAX + ((income - c_second) * ((double)p_third / 100));
-	else if (income < c_fourth)
-		total_tax = TIER_10_TAX + TIER_12_TAX + TIER_22_TAX + ((income - c_third) * ((double)p_third / 100));
+	if (user_income < c_first)
+		total_tax = user_income * ((double)p_first / 100);
+	else if (user_income < c_second)
+		total_tax = TIER_10_TAX + ((user_income - c_first) * ((double)p_second / 100));
+	else if (user_income < c_third)
+		total_tax = TIER_10_TAX + TIER_12_TAX + ((user_income - c_second) * ((double)p_third / 100));
+	else if (user_income < c_fourth)
+		total_tax = TIER_10_TAX + TIER_12_TAX + TIER_22_TAX + ((user_income - c_third) * ((double)p_third / 100));
 	else
-		total_tax = TIER_10_TAX + TIER_12_TAX + TIER_22_TAX + TIER_24_TAX + ((income - c_fourth) * ((double)p_fourth / 100));
-	return total_tax;
-}
+		total_tax = TIER_10_TAX + TIER_12_TAX + TIER_22_TAX + TIER_24_TAX + ((user_income - c_fourth) * ((double)p_fourth / 100));
 
+	printf("Your total taxes are: $%0.2f\n", total_tax);
+}
